e7c09af8cb6659259d41a9d110c60149e5c8659a
Jahnawi <jahnawiagrawal@gmail.com>
2026-02-09 15:20:54 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/ratelimiter/gateway/RateLimiterGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ratelimiter.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimiterGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimiterGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratelimiter/gateway/RateLimiterGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratelimiter.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimiterGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
